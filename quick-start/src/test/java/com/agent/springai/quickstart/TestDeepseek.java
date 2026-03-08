985a5afc9b0d4b15e1e0f800a8c4519f2ad4487b
SaeMyte <tonia.zhu@outlook.com>
2026-03-08 22:30:27 +0800

    [feat]测试deepseek通用模型配置及深度思考功能

--- quick-start/src/test/java/com/agent/springai/quickstart/TestDeepseek.java
+++ quick-start/src/test/java/com/agent/springai/quickstart/TestDeepseek.java
@@ -1,7 +1,11 @@
 package com.agent.springai.quickstart;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.deepseek.DeepSeekAssistantMessage;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
@@ -18,5 +22,31 @@ public class TestDeepseek {
     public void testDeepseekStream(@Autowired DeepSeekChatModel deepSeekChatModel){
         Flux<String> stream =  deepSeekChatModel.stream("你好你是谁");
         stream.toIterable().forEach(System.out::println);
+    }
+
+    /**
+     * 测试模型通用配置
+     *
+     * @param deepSeekChatModel deepseek模型实例
+     */
+    @Test
+    public void testChatOptions(@Autowired DeepSeekChatModel deepSeekChatModel){
+        DeepSeekChatOptions options = DeepSeekChatOptions.builder().model("deepseek-chat").temperature(1.9d).build();
+        ChatResponse chatResponse = deepSeekChatModel.call(new Prompt("请写一首诗描述清晨",options));
+        System.out.println(chatResponse.getResult().getOutput().getText());
+    }
+
+    /**
+     * 测试deepseek推理模型深度思考功能
+     *
+     * @param deepSeekChatModel deepseek模型实例
+     */
+    @Test
+    public void testDeepseekThinking(@Autowired DeepSeekChatModel deepSeekChatModel){
+        Prompt prompt = new Prompt("你好你是谁");
+        ChatResponse chatResponse =  deepSeekChatModel.call(prompt);
+        DeepSeekAssistantMessage output = (DeepSeekAssistantMessage) chatResponse.getResult().getOutput();
+        System.out.println(output.getReasoningContent());
+        System.out.println(output.getText());
     }
 }
